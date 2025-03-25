7b8ec4f1d96e2759a0c13a14d37d667a0c198862
Amaury PM <amaury.pema@gmail.com>
2025-03-25 10:34:09 -0600

    Archivo GameDatabase.kt agregado al repo y AGP actualizado

--- app/src/main/java/com/amaurypm/videogamesdb/ui/GameAdapter.kt
+++ app/src/main/java/com/amaurypm/videogamesdb/ui/GameAdapter.kt
@@ -5,14 +5,14 @@ import androidx.recyclerview.widget.RecyclerView
 
 class GameAdapter(): RecyclerView.Adapter<GameViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
-
+        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
-
+        TODO("Not yet implemented")
     }
 
     override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
-
+        TODO("Not yet implemented")
     }
 }
